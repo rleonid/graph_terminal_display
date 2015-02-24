0d912bc4a958309c4883f6d2e1cc93cfe0ef246b
Leonid Rozenberg <leonidr@gmail.com>
2015-02-24 15:04:34 -0500

    Corrected the flowing X/Y constraint formulation.

--- position.ml
+++ position.ml
@@ -58,28 +58,29 @@ let pltia = predecessor_list_to_indexed_arr
    Even though, other's may have precedense on the first element it will be
    displayed on top.
    *)
-let with_predecessors msg_arr terminal_width =
+let with_predecessors msg_arr terminal_width display_width =
   let num  = Array.length msg_arr in
   let x_lb = 0 and x_ub = terminal_width in
-  let y_lb = 0 and y_ub = num * 2 in              (* Flix XY over X axis *)
+  let y_lb = 0 and y_ub = num * 2 in              (* Flip XY over X axis *)
   (* Variables *)
   let xpos = Fd.array ~name:"xpos" num x_lb x_ub in
   let ypos = Fd.array ~name:"ypos" num y_lb y_ub in
   (* Constraints *)
+  Cstr.post (Alldiff.cstr ypos);
+  Cstr.post (Alldiff.cstr xpos);
   Cstr.post (fd2e ypos.(0) =~ i2e 0);     (* First on top. *)
   for i = 0 to num - 1 do
-    let others = snd msg_arr.(i) in
+    let me,others = msg_arr.(i) in
     (* 'On Top' means _lower_ y position. *)
+    let len = i2e (String.length me + display_width) in
     List.iter (fun oi ->
-        let len = i2e (String.length (fst msg_arr.(oi))) in
         if oi <> 0 then Cstr.post (fd2e ypos.(i) <~ fd2e ypos.(oi));
-        Cstr.post (fd2e xpos.(i) <~ fd2e xpos.(oi) -~ len))
+        Cstr.post (fd2e xpos.(i) +~ len <~ fd2e xpos.(oi)))
         others
   done;
-  Cstr.post (Alldiff.cstr ypos);
-  let value = Fd.elt_value in
-  let targ  = Array.append xpos ypos in
+  let targ = Array.append xpos ypos in
   if Goals.solve (Goals.Array.labeling targ) then
+    let value = Fd.elt_value in
     let rec loop i acc =
       if i >= num then acc
       else loop (i + 1) ((value xpos.(i), value ypos.(i), (fst msg_arr.(i)))::acc)
@@ -90,9 +91,11 @@ let with_predecessors msg_arr terminal_width =
 
 let display lst =
   List.sort (fun (_x1, y1, _l1) (_x2, y2, _l2) -> compare y1 y2) lst
-  |> List.iter (fun (x, _, l) -> Printf.printf "%*s%s\n" x " " l)
+  |> List.iter (fun (x, _, l) ->
+      if x = 0 then Printf.printf "%s\n" l
+               else Printf.printf "%*s%s\n" x " " l)
 
 let layout arr =
-  display (with_predecessors arr 80)
+  display (with_predecessors arr 80 2)
 
 let _ = layout [| "foo",[2;3]; "bar",[0;2]; "dog",[3] ; "cat",[]|] ;;
